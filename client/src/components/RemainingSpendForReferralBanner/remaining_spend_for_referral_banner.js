65d4c3697ea03073237321cafe4939433e148e29
Uropt <lisovetskyi.b.v@gmail.com>
2021-03-03 19:00:59 +0200

    fix some error

--- client/src/components/RemainingSpendForReferralBanner/remaining_spend_for_referral_banner.js
+++ client/src/components/RemainingSpendForReferralBanner/remaining_spend_for_referral_banner.js
@@ -5,28 +5,28 @@ function RemainingSpendForReferralBanner() {
   const userData = useSelector(store => (store?.auth ?? {})) ?? ''
   const user = userData?.user
 
-  if (user.referrer && user.amount_spent < 20000) {
+  if (user?.referrer && user?.amount_spent < 20000) {
     return (
       <div bp="12" className="remaining-spend">
         <p bp="margin-bottom">
-          You were referred by {user.referrer}. Once you spend $200, you will earn $25 in lead
+          You were referred by {user?.referrer}. Once you spend $200, you will earn $25 in lead
           credit.
         </p>
 
         <div className="progress-bar">
           <div
             className="progress-bar-filled"
-            style={{ width: `${(user.amount_spent / 20000) * 100}%` }}
+            style={{ width: `${(user?.amount_spent / 20000) * 100}%` }}
           />
           <div
             className="progress-bar-circle"
-            style={{ left: `calc(${(user.amount_spent / 20000) * 100}% - 6px)` }}
+            style={{ left: `calc(${(user?.amount_spent / 20000) * 100}% - 6px)` }}
           />
           <div
             className="progress-bar-amount"
-            style={{ left: `calc(${(user.amount_spent / 20000) * 100}% - 11px)` }}
+            style={{ left: `calc(${(user?.amount_spent / 20000) * 100}% - 11px)` }}
           >
-            ${Math.floor(user.amount_spent / 100).toFixed(0)}
+            ${Math.floor(user?.amount_spent / 100).toFixed(0)}
           </div>
         </div>
       </div>
